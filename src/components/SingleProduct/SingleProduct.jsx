6cafebba85978f1ae386fbb0ef2323fd507cfe77
Mavludin <imevlud@gmail.com>
2025-03-03 18:07:51 +0300

    refactor

--- src/components/SingleProduct/SingleProduct.jsx
+++ src/components/SingleProduct/SingleProduct.jsx
@@ -1,20 +1,32 @@
 import { useParams } from "react-router-dom";
-import { useGetProductByIdQuery } from "../../store/slices/productsApi";
+import { useGetProductByCategoryAndIdQuery } from "../../store/slices/productsApi";
 import Similar from "./Similar";
 import ScrollToTop from "../../config";
 
 const SingleProduct = () => {
-  const params = useParams();
+  const { category, id } = useParams();
 
-  const { data } = useGetProductByIdQuery(params.id);
+  const { data, isLoading } = useGetProductByCategoryAndIdQuery({ category, id });
 
   ScrollToTop();
+
+  if (isLoading === true) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (isLoading === false && !data) {
+    return null;
+  }
+
+  const item = data[0]
+
+  if (!item) return null
 
   return (
     <div className="container">
       <div className="single-product">
         <div className="single-product-image">
-          <img src={data?.[0].image} alt={data?.[0].title} />
+          <img src={item.image} alt={item.title} />
           <div className="single-product-icons">
             <button className="like">
               <img src="../icons/heart.svg" alt="like" />
@@ -27,26 +39,26 @@ const SingleProduct = () => {
           </div>
         </div>
         <div className="single-product-details">
-          <h1 className="single-product-title">{data?.[0].title}</h1>
+          <h1 className="single-product-title">{item.title}</h1>
           <span className="single-product-rating">{`4.${Math.random()
             .toString()
             .split(".")[1]
             .substring(0, 1)}`}</span>
-          <p className="single-product-descr">{data?.[0].description}</p>
+          <p className="single-product-descr">{item.description}</p>
           <table>
             <tbody>
               <tr>
                 <td>Brand</td>
-                <td>{data?.[0].brand}</td>
+                <td>{item.brand}</td>
               </tr>
               <tr>
                 <td>Model</td>
-                <td>{data?.[0].model}</td>
+                <td>{item.model}</td>
               </tr>
-              {data?.[0].color && (
+              {item.color && (
                 <tr>
                   <td>Color</td>
-                  <td>{data?.[0].color}</td>
+                  <td>{item.color}</td>
                 </tr>
               )}
             </tbody>
@@ -55,24 +67,24 @@ const SingleProduct = () => {
         <div>
           <div className="single-product-purchase">
             <div className="single-product-price">
-              {data?.[0].onSale ? (
+              {item.onSale ? (
                 <>
                   <div className="prices">
                     <span className="single-product-new-price">
                       $
-                      {data?.[0].price -
-                        (data?.[0].price / 100) * data?.[0].discount}
+                      {item.price -
+                        (item.price / 100) * item.discount}
                     </span>
                     <span className="single-product-old-price">
-                      last price $ {data?.[0].price}
+                      last price $ {item.price}
                     </span>
                   </div>
                   <span className="single-product-discount">
-                    -{data?.[0].discount}%
+                    -{item.discount}%
                   </span>
                 </>
               ) : (
-                `$ ${data?.[0].price}`
+                `$ ${item.price}`
               )}
             </div>
             <div className="single-product-buttons">
@@ -83,19 +95,20 @@ const SingleProduct = () => {
           <ul className="single-product-advantages">
             <li className="in-stock">In Stock</li>
             <li className="guaranteed">Guaranteed</li>
-            {!data?.[0].onSale && data?.[0].price > 1000 && (
+            {!item.onSale && item.price > 1000 && (
               <li className="delivery">Free Delivery</li>
             )}
-            {data?.[0].onSale &&
-              data?.[0].price - (data?.[0].price / 100) * data?.[0].discount >
+            {item.onSale &&
+              item.price - (item.price / 100) * item.discount >
                 1000 && <li className="delivery">Free Delivery</li>}
           </ul>
         </div>
       </div>
+
       <Similar
-        category={data?.[0].category}
-        brand={data?.[0].brand}
-        id={data?.[0].id}
+        category={item.category}
+        brand={item.brand}
+        id={item.id}
       />
     </div>
   );
